c43ed70427d9b2e46ee3aedcbaaf613a11ab8117
philippos <yuufjt@gmail.com>
2015-12-22 13:08:43 +0900

    まあこんな感じで

--- game.rb
+++ game.rb
@@ -6,7 +6,7 @@ class Game
   Game::BLACK = 2
   Game::WALL  = 3
 
-  attr_accessor :board, :turn, :turn_count, :whites, :blacks
+  attr_reader :board
 
   def initialize
     width = ["A", "B", "C", "D", "E", "F", "G", "H"]
@@ -23,10 +23,17 @@ class Game
     @board[Game::E4] = Game::BLACK
     @board[Game::E5] = Game::WHITE
 
-    @turn = Game::WHITE
-    @turn_count = 0
-    @whites = 2
-    @blacks = 2
+    @values = [
+      0,   0,   0,  0,  0,  0,  0,   0,   0,
+      0,  30, -12,  0, -1, -1,  0, -12,  30,
+      0, -12, -15, -3, -3, -3, -3, -15, -12,
+      0,   0,  -3,  0, -1, -1,  0,  -3,   0,
+      0,  -1,  -3, -1, -1, -1, -1,  -3,  -1,
+      0,  -1,  -3, -1, -1, -1, -1,  -3,  -1,
+      0,   0,  -3,  0, -1, -1,  0,  -3,   0,
+      0, -12, -15, -3, -3, -3, -3, -15, -12,
+      0,  30, -12,  0, -1, -1,  0, -12,  30,
+    ]
   end
 
   def display
@@ -40,50 +47,105 @@ class Game
     end
   end
 
+  def ai_turn(turn:)
+    blanks = @board.each_with_index.select {|kind, index| kind == Game::BLANK }.map {|n| n.last }
+
+    valuations = blanks.map {|index|
+      [index, test(turn: turn, index: index).map {|target| @values[target] }.inject(:+)]
+    }.select {|valuation|
+      valuation.last
+    }.sort_by {|valuation| valuation.last }
+
+    return false if valuations.empty?
+
+    flip(turn: turn, index: valuations.last.first)
+
+    true
+  end
+
   def flip(turn:, index:)
-    [-10, -9, -8, -1, 1, 8, 9, 10].each do |dir|
-      flip_line(turn: turn, index: index, dir: dir)
-    end
+    targets = test(turn: turn, index: index)
+
+    return false unless targets.length > 0
 
     @board[index] = turn
+    targets.each do |target|
+      @board[target] = turn
+    end
+
+    true
+  end
+
+  def test(turn:, index:)
+    targets = [-10, -9, -8, -1, 1, 8, 9, 10].map do |dir|
+      test_line(turn: turn, index: index, dir: dir)
+    end
+    targets.flatten!
+    targets.unshift(index) unless targets.empty?
+    targets
   end
 
   private
 
-  def flip_line(turn:, index:, dir:)
+  def test_line(turn:, index:, dir:)
     target = index + dir
 
     while board[target] == ((turn == Game::WHITE) ? Game::BLACK : Game::WHITE) do
       target += dir
     end
 
-    return 0 if board[target] != turn
+    return [] if board[target] != turn
 
-    flips = 0
+    targets = []
     target -= dir
     while target != index do
-      board[target] = turn
+      targets << target
       target -= dir
-      flips += 1
     end
-
-    flips
+    targets
   end
-
 end
+
+#### main ####
 
 game = Game.new
 game.display
 
+loop do
+  blanks = game.board.select {|index| index == Game::BLANK }.length
+  blacks = game.board.select {|index| index == Game::BLACK }.length
+  whites = game.board.select {|index| index == Game::WHITE }.length
 
-game.flip(turn: Game::BLACK, index: Game::D3)
-game.display
+  puts "blacks: #{blacks} whites: #{whites}"
 
-game.flip(turn: Game::WHITE, index: Game::E3)
-game.display
+  if blanks == 0
+    if blacks > whites
+      puts "you win !"
+    elsif blacks == whites
+      puts "even."
+    else
+      puts "you lose."
+    end
+    exit
+  end
 
-game.flip(turn: Game::BLACK, index: Game::F3)
-game.display
+  print "your turn. you are BLACK(X) > "
+  got = gets.chomp
 
+  next unless got.match(/((A|B|C|D|E|F|G|H)(1|2|3|4|5|6|7|8)|quit)/)
+  exit if got == "quit"
 
+  locate = eval("Game::#{got}")
+  next unless game.test(turn: Game::BLACK, index: locate).length > 0
+
+  puts "\n==== your choice ===="
+  game.flip(turn: Game::BLACK, index: locate)
+  game.display
+
+  puts "\n==== AI turn ===="
+  result = game.ai_turn(turn: Game::WHITE)
+  game.display
+
+  puts "\n==== AI turn have passed. ====" unless result
+end
 
